0608f53ce767f8cb45b1084dd8109d85d8a0111d
luistaype <ing.luis.taype@gmail.com>
2016-04-24 17:58:12 -0500

    Clase 01

---
+++ Clase01/Demo001/src/pe/egcc/app/service/MateService.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pe.egcc.app.service;
+
+/**
+ *
+ * @author Alumno
+ */
+public class MateService 
+{
+    
+    public int sumar(int n1, int n2)
+    {
+        return (n1+n2);
+    }
+    
+    
+            
+         
+    
+}
